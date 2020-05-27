3686f79a027eed357b0923211086729da3e46aa3
Hilton Santos <jhilton930@gmail.com>
2020-05-27 13:04:34 -0300

    Update Covid-19 App

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,7 @@
+import 'package:covidinfo/screens/screen_about.dart';
+import 'package:covidinfo/screens/screen_brazil.dart';
+import 'package:covidinfo/screens/screen_news.dart';
+import 'package:covidinfo/screens/screen_world.dart';
 import 'package:flutter/material.dart';
 import 'package:covidinfo/screens/home.dart';
 
@@ -6,8 +10,14 @@ void main() {
     MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Covid-19 Info',
-      home: Home(),
-
+      initialRoute: '/',
+      routes: {
+        '/': (context) => Home(),
+        'sc_about': (context) => buildPageAbout(),
+        'sc_world': (context) => buildPageWorld(),
+        'sc_news': (context) => buildPageNews(),
+        'sc_brazil': (context) => buildPageBrazil()
+      },
     ),
   );
 }
